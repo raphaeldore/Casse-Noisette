6153e0fb3950df9b0c039e4fb46b906d5918a502
Raphaël Doré <rdore@neomailbox.ch>
2015-04-29 19:45:47 -0400

    Création de la HashFactory.

--- CasseNoisette/Crypto/HashFactory.cpp
+++ CasseNoisette/Crypto/HashFactory.cpp
@@ -1,3 +1,41 @@
 #include "stdafx.h"
+#include "HashFactory.h"
 
 using namespace CustomCrypto;
+
+map<string, unique_ptr<IHash>> HashFactory::hashFunctions_map;
+
+HashFactory::HashFactory()
+{
+	// Les algorithmes de hachage qu'on supporte pour l'instant.
+	// C'est très facile d'en ajouter.
+	Register("MD5", make_unique<MD5>());
+	Register("SHA-1", make_unique<SHA1>());
+	Register("SHA-256", make_unique<SHA256>());
+}
+
+HashFactory::~HashFactory()
+{
+}
+
+HashFactory * HashFactory::GetHashFactory()
+{
+	static unique_ptr<HashFactory> factoryInstance = make_unique<HashFactory>();
+	return factoryInstance.get();
+}
+
+void HashFactory::Register(const string _hashName, unique_ptr<IHash> _hashFunction)
+{
+	if (hashFunctions_map.find(_hashName) == hashFunctions_map.end())
+	{
+		hashFunctions_map[_hashName] = move(_hashFunction);
+	}
+}
+
+IHash * HashFactory::CreateHashAlgorithm(const string& _hashName) const
+{
+	if (hashFunctions_map.find(_hashName) == hashFunctions_map.end())
+		throw invalid_argument(string(__FUNCTION__) + ": unknown hash algorithm");
+
+	return hashFunctions_map.at(_hashName).get();
+}
--- CasseNoisette/Crypto/HashFactory.h
+++ CasseNoisette/Crypto/HashFactory.h
@@ -1,11 +1,24 @@
 #pragma once
 #include "stdafx.h"
 
+using namespace std;
+
 namespace CustomCrypto
 {
+	class IHash;
+
 	class HashFactory
 	{
 	public:
+		HashFactory();
+		virtual ~HashFactory();
+
+		static HashFactory * GetHashFactory();
+
+		static void Register(const string _hashName, unique_ptr<IHash> _hashFunction);
+		IHash * CreateHashAlgorithm(const string & _hashName) const;
+
 	private:
+		static map<string, unique_ptr<IHash>> hashFunctions_map;
 	};
 }
