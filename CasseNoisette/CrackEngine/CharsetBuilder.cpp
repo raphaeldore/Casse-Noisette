4bbfa7afc57b5f002482f9d6c15670a59d4cca5f
Raphaël Doré <rdore@neomailbox.ch>
2015-05-01 21:08:38 -0400

    CharsetBuilder gère maintenant les caractères fournis par l'utilisateur.

--- CasseNoisette/CrackEngine/CharsetBuilder.cpp
+++ CasseNoisette/CrackEngine/CharsetBuilder.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "CharsetBuilder.h"
+#include <algorithm>
 
 using namespace CrackEngine;
 
@@ -20,10 +21,12 @@ CharsetBuilder::CharsetBuilder()
 	numeric = false;
 	space = false;
 	special = false;
+	customCharset = "";
 }
 
-CharsetBuilder::CharsetBuilder(const bool _loweralpha, const bool _upperalpha, const bool _numeric, const bool _space, const bool _special)
+CharsetBuilder::CharsetBuilder(const bool _loweralpha, const bool _upperalpha, const bool _numeric, const bool _space, const bool _special, const string & _customCharset)
 {
+	customCharset = _customCharset;
 	loweralpha = _loweralpha;
 	upperalpha = _upperalpha;
 	numeric = _numeric;
@@ -56,6 +59,11 @@ void CharsetBuilder::enableSpecial()
 	special = true;
 }
 
+void CharsetBuilder::addCustomCharset(const string& _customCharset)
+{
+	customCharset = _customCharset;
+}
+
 string CharsetBuilder::BuildCharset()
 {
 	string generatedCharset = "";
@@ -66,5 +74,17 @@ string CharsetBuilder::BuildCharset()
 	if (space) generatedCharset.append(Charsets::space);
 	if (special) generatedCharset.append(Charsets::special);
 
+	if (!customCharset.empty()) {
+		generatedCharset.append(customCharset);
+		// Au cas où l'utilisateur ajoute un charactère qui existe deja.
+		removeDuplicateCharacters(generatedCharset);
+	}
+
 	return generatedCharset;
+}
+
+void CharsetBuilder::removeDuplicateCharacters(string& _generatedCharset)
+{
+	sort(_generatedCharset.begin(), _generatedCharset.end());
+	_generatedCharset.erase(unique(_generatedCharset.begin(), _generatedCharset.end()));
 }
--- CasseNoisette/CrackEngine/CharsetBuilder.h
+++ CasseNoisette/CrackEngine/CharsetBuilder.h
@@ -6,7 +6,7 @@ namespace CrackEngine
 	{
 	public:
 		CharsetBuilder();
-		CharsetBuilder(const bool _loweralpha, const bool _upperalpha, const bool _numeric, const bool _space, const bool _special);
+		CharsetBuilder(const bool _loweralpha, const bool _upperalpha, const bool _numeric, const bool _space, const bool _special, const string & _customCharset = "");
 
 		// Si l'utilisateur préfère ne pas passer par le constructeur
 		void enableLowerAlpha();
@@ -14,14 +14,20 @@ namespace CrackEngine
 		void enableNumeric();
 		void enableSpace();
 		void enableSpecial();
+		void addCustomCharset(const string & _customCharset);
 
 		string BuildCharset();
 
 	private:
+
+		void removeDuplicateCharacters(string & _generatedCharset);
+
 		bool loweralpha;
 		bool upperalpha;
 		bool numeric;
 		bool space;
 		bool special;
+
+		string customCharset;
 	};
 }
