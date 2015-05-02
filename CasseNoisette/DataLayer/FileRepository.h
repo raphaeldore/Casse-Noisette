4d64cacb1da8aaee80e2f88b71717bdf47f7cd30
Raphaël Doré <rdore@neomailbox.ch>
2015-05-02 15:31:10 -0400

    Ajout TODO.

--- CasseNoisette/DataLayer/FileRepository.h
+++ CasseNoisette/DataLayer/FileRepository.h
@@ -1,19 +1,27 @@
 #pragma once
 #include "stdafx.h"
 
+/*
+
+TODO:
+    - Validation du contenu du fichier
+    - Utilisation d'un séparateur (ex: username:hashedPassword)
+    - Validation du formatage de chaque lignes du fichier
+    - Bref, de la robustesse.
+*/
 
 using namespace std;
 
 namespace DataLayer
 {
-	class FileRepository
-	{
-	public:
-		void loadPasswordFile(const string _pwdFilePath);
-		vector<string> getAllHashedPasswords() const;
-	private:
-		vector<string> hashedPasswords;
-		bool fileIsEmpty(ifstream & _file) const;
-	};
+    class FileRepository
+    {
+    public:
+        void loadPasswordFile(const string _pwdFilePath);
+        vector<string> getAllHashedPasswords() const;
+    private:
+        vector<string> hashedPasswords;
+        bool fileIsEmpty(ifstream & _file) const;
+    };
 
 }
