5c642730cfd5e583f681a17e83046b1c14449711
Raphaël Doré <rdore@neomailbox.ch>
2015-05-05 13:28:26 -0400

    ICrackEngine: Ajout SetHashedPasswords et modification setPasswordHashFunction.
    SetHashedPasswords accepte un vecteur de mots de passe.
    SetPasswordHashFunction s'occupe d'instancier la bonne fonction de
    hachage.

--- CasseNoisette/CrackEngine/ICrackEngine.h
+++ CasseNoisette/CrackEngine/ICrackEngine.h
@@ -7,15 +7,14 @@ namespace CrackEngine
 	public:
 		virtual ~ICrackEngine() {}
 		virtual void Crack() = 0;
-		void setPwdFilePath(const string & _pwdFilePath);
+		void setHashedPasswords(const vector<string> & _hashedPasswords);
 		void setResultsFilePath(const string & _resultsFilePath);
 		void setPwdHashFunction(const string & _pwdHashFunction);
 		vector<string> getResults();
 	protected:
-		string pwdFilePath;
+		CustomCrypto::IHash * hashAlgorithm;
 		string resultsFilePath;
-		string pwdHashFunction;
-
+		vector<string> hashedPasswords;
 		vector<string> results;
 	};
 }
