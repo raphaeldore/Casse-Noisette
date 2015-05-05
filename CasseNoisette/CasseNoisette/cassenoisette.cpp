ab4293c9392dc6510d50f4cd97a44765720476b9
Raphaël Doré <rdore@neomailbox.ch>
2015-05-04 21:56:22 -0400

    Ajout éléments manquants pour le sprint 0. Sprint 0 complété!

--- CasseNoisette/CasseNoisette/cassenoisette.cpp
+++ CasseNoisette/CasseNoisette/cassenoisette.cpp
@@ -1,5 +1,12 @@
 #include "cassenoisette.h"
 #include <iostream>
+#include "../CrackEngine/Parameter.h"
+#include "../CrackEngine/CrackFactoryParams.h"
+#include "../CrackEngine/CrackFactory.h"
+
+#include <QMessageBox>
+
+#include <thread>
 
 CasseNoisette::CasseNoisette(QWidget *parent)
 	: QMainWindow(parent)
@@ -14,6 +21,53 @@ CasseNoisette::~CasseNoisette()
 
 void CasseNoisette::on_startCrackBtn_clicked()
 {
+	using namespace CrackEngine;
+	
+	ui.tabWidget->currentIndex();
+
+	CrackFactoryParams crackFactoryParams;
+	crackFactoryParams.addParameter(Parameter(PWD_FILE_PATH, ui.pwdFileSelectTxt->text().toStdString()));
+	crackFactoryParams.addParameter(Parameter(RESULTS_FILE_PATH, "chemin/bidon/fichier_bidon.txt"));
+	crackFactoryParams.addParameter(Parameter(CHARSET, "abcdefghijklmnopqrstuvwxyz"));
+	crackFactoryParams.addParameter(Parameter(MAX_PWD_LENGTH, ui.spinBox->text().toStdString()));
+	crackFactoryParams.addParameter(Parameter(HASH_TYPE, ui.hashFunctionsComboBox->currentText().toStdString()));
+
+	unique_ptr<ICrackEngine> crackEngine;
+	QMessageBox errorBox;
+	errorBox.setText("Il y a eut une erreur avec vos choix");;
+
+	try
+	{
+		crackEngine = CrackFactory::GetCrackFactory()->CreateCrackEngine(BRUTE_FORCE, crackFactoryParams);
+	} catch (const runtime_error & err)
+	{
+		std::cerr << err.what();
+		errorBox.exec();		
+	} catch(const exception & ex)
+	{
+		std::cerr << ex.what();
+		errorBox.exec();
+	}
+
+	ui.startCrackBtn->setEnabled(false);
+
+	crackEngine->Crack();
+
+	ui.startCrackBtn->setEnabled(true);
+
+	QString passwords("Mots de passe trouvés:");
+
+	for (auto pass : crackEngine->getResults())
+	{
+		//passwords.append("\n" + pass);
+		passwords += "\n";
+		passwords += QString(pass.c_str());
+	}
+
+	QMessageBox msgBox;
+	QString message("Le cassage des mots de passe est terminé!");
+	msgBox.setText(passwords);
+	msgBox.exec();
 }
 
 void CasseNoisette::on_pwdFileSelectBtn_clicked()
