75b76aabcda81e5474f86b5f2976fd2b1ee2754d
Raphaël Doré <rdore@neomailbox.ch>
2015-05-11 17:48:09 -0400

    ajout commentaire explicatif

--- CasseNoisette/DataLayer/FileRepository.cpp
+++ CasseNoisette/DataLayer/FileRepository.cpp
@@ -58,6 +58,7 @@ unique_ptr<queue<string>> FileRepository::loadDictionaryFile(const string& _dict
 
 	for (uint64_t i = 0; i < dictionaryFile.size(); i++)
 	{
+		// On ignore les fin de lignes et les retours de chariot (1 mot par ligne)
 		if (buffer[i] == '\n' || buffer[i] == '\r') continue;
 
 		string line;
