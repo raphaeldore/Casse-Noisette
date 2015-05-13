76d2f5a94cc4c2d9f138f5f52a3e6c75f8fce885
Raphaël Doré <rdore@neomailbox.ch>
2015-05-12 20:27:51 -0400

    FileUtilities fait maintenant partie du namespace Utilities.

--- CasseNoisette/Utilities/FileUtilities.cpp
+++ CasseNoisette/Utilities/FileUtilities.cpp
@@ -2,6 +2,7 @@
 #include "FileUtilities.h"
 
 using namespace std;
+using namespace Utilities;
 
 FileUtilities::FileUtilities()
 {
--- CasseNoisette/Utilities/FileUtilities.h
+++ CasseNoisette/Utilities/FileUtilities.h
@@ -1,22 +1,22 @@
 /*
 
 FileUtilities: Un petite classe avec une couple de méthodes statiques
-               qui offre quelques fonctions bien pratique pour intéragir
-			   avec des fichiers.
+qui offre quelques fonctions bien pratique pour intéragir
+avec des fichiers.
 
 @author Raphaël Doré
 
 */
 
+namespace Utilities {
 #pragma once
-class FileUtilities
-{
-public:
-	FileUtilities();
-	~FileUtilities();
+	class FileUtilities
+	{
+	public:
+		FileUtilities();
+		~FileUtilities();
 
-	static bool DoesFileExist(const string & _path);
-	static bool CreateFile(const string & _path, const bool & _overwriteExisting = false);
-
-};
-
+		static bool DoesFileExist(const string & _path);
+		static bool CreateFile(const string & _path, const bool & _overwriteExisting = false);
+	};
+}
