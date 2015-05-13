b3bd1e2ec3737aae41661bf3684b563d7e6eb5e9
Raphaël Doré <rdore@neomailbox.ch>
2015-05-13 09:12:54 -0400

    tabs to space

--- CasseNoisette/CasseNoisette/cassenoisette.cpp
+++ CasseNoisette/CasseNoisette/cassenoisette.cpp
@@ -76,8 +76,8 @@ string CasseNoisette::GetCharset() const
 		ui.chkCharsetSpace->isChecked(),
 		ui.chkCharsetSpecial->isChecked(),
 		ui.txtCustomCharset->text().toLocal8Bit().constData() // Les QString sont en UTF-16, 
-															  // et les std::string sont en UTF-8. 
-															  // On doit les convertir.
+                                                              // et les std::string sont en UTF-8. 
+                                                              // On doit les convertir.
 	};
 
 	return charsetBuilder.BuildCharset();
