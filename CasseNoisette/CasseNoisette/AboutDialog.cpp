597a62ae9e47099b9680761352115fda97a42a9c
Dominique Septembre <dominique.septembre@gmail.com>
2015-05-15 08:55:48 -0400

    Adding logo on about window

--- CasseNoisette/CasseNoisette/AboutDialog.cpp
+++ CasseNoisette/CasseNoisette/AboutDialog.cpp
@@ -5,6 +5,8 @@ AboutDialog::AboutDialog(QWidget * parent)
 	:QDialog(parent)
 {
 	ui.setupUi(this);
+	this->setModal(true);
+	this->setWindowFlags(this->windowFlags() & ~Qt::WindowContextHelpButtonHint);
 }
 
 void AboutDialog::on_closeBtn_clicked(){
