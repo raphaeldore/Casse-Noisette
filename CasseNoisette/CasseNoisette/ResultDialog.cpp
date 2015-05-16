758813eaaf959570356045935a36e442af3460a9
Raphaël Doré <rdore@neomailbox.ch>
2015-05-15 22:34:01 -0400

    Ajout méthodes setCrackingResults, et resultTupleToString. S'occupe de formater les résultats.

--- CasseNoisette/CasseNoisette/ResultDialog.cpp
+++ CasseNoisette/CasseNoisette/ResultDialog.cpp
@@ -16,6 +16,32 @@ void ResultDialog::setInformativeText(string informativeText)
 	ui.informativeTxt->setPlainText(QString::fromStdString(informativeText));
 }
 
+void ResultDialog::setCrackingResults(vector<tuple<string, string, string>> _results, const QString & _crackingTime)
+{
+	QString passwords_found_message;
+	if (_results.size() == 1)
+	{
+		passwords_found_message = "1 mot de passe trouvé:\n\n" + resultTupleToString(_results.front());
+	}
+	else if (_results.size() > 1)
+	{
+		passwords_found_message = QString::number(_results.size()) + " mots de passe trouvés:\n ";
+		for (auto pass : _results)
+		{
+			passwords_found_message += "\n" + resultTupleToString(pass);
+		}
+	}
+	else
+	{
+		passwords_found_message = "Aucun mot de passe trouvé";
+	}
+
+	passwords_found_message += "\n\nTemps total: " + _crackingTime + " secs";
+
+	ui.resultTxt->setPlainText(passwords_found_message);
+
+}
+
 void ResultDialog::setResultText(QString resultText)
 {
 	ui.resultTxt->setPlainText(resultText);
@@ -35,4 +61,13 @@ void ResultDialog::on_saveBtn_clicked()
 	stream << ui.resultTxt->toPlainText();
 	f.flush();
 	f.close();
+}
+
+QString ResultDialog::resultTupleToString(const tuple<string, string, string>& _resultTuple) const
+{
+	QString userName = QString::fromLocal8Bit(get<0>(_resultTuple).c_str());
+	QString hashedPassword = QString::fromLocal8Bit(get<1>(_resultTuple).c_str());
+	QString plainTextPassword = QString::fromLocal8Bit(get<2>(_resultTuple).c_str());
+
+	return userName + " : " + hashedPassword + " : " + plainTextPassword;
 }
--- CasseNoisette/CasseNoisette/ResultDialog.h
+++ CasseNoisette/CasseNoisette/ResultDialog.h
@@ -14,11 +14,14 @@ class ResultDialog : public QDialog{
 public:
 	ResultDialog(QWidget * parent = 0);
 	void setInformativeText(string informativeText);
+	void setCrackingResults(vector<tuple<string, string, string>> _results, const QString & _crackingTime);
 	void setResultText(QString resultText);
 	public slots:
 	void on_closeBtn_clicked();
 	void on_saveBtn_clicked();
 private:
+	QString resultTupleToString(const tuple<string, string, string> & _resultTuple) const;
+
 	Ui::ResultDialog ui;
 };
 
