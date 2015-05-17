6cabefa85fd692a34efdfc6a4b31d15b556545fd
Raphaël Doré <rdore@neomailbox.ch>
2015-05-17 11:10:36 -0400

    REsultDialog: using namespace std passe du .h au .cpp

--- CasseNoisette/CasseNoisette/ResultDialog.h
+++ CasseNoisette/CasseNoisette/ResultDialog.h
@@ -3,24 +3,23 @@
 
 #include "ui_resultdialog.h"
 
-#include <QtWidgets/QDialog>
-
-using namespace std;
 
 class QCheckBox;
 
-class ResultDialog : public QDialog{
+class ResultDialog : public QDialog
+{
 	Q_OBJECT
+
 public:
-	ResultDialog(QWidget * parent = 0);
-	void setInformativeText(string informativeText);
-	void setCrackingResults(vector<tuple<string, string, string>> _results, const QString & _crackingTime);
+	ResultDialog(QWidget* parent = 0);
+	void setInformativeText(std::string informativeText);
+	void setCrackingResults(std::vector<std::tuple<std::string, std::string, std::string>> _results, const QString& _crackingTime);
 	void setResultText(QString resultText);
-	public slots:
+public slots:
 	void on_closeBtn_clicked();
 	void on_saveBtn_clicked();
 private:
-	QString resultTupleToString(const tuple<string, string, string> & _resultTuple) const;
+	QString resultTupleToString(const std::tuple<std::string, std::string, std::string>& _resultTuple) const;
 
 	Ui::ResultDialog ui;
 };
