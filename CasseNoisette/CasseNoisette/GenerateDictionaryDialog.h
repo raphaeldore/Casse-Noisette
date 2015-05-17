d78be51aa80333f9ff761bb572673e40528208c9
Raphaël Doré <rdore@neomailbox.ch>
2015-05-17 11:05:25 -0400

    Réorganisation de GenerateDictionaryDialog.h.
    Utilisation de forward decleration, ajout de commentaires, changement de l'ordre des méthodes selon leur accessibilité (public, protected, private).

--- CasseNoisette/CasseNoisette/GenerateDictionaryDialog.h
+++ CasseNoisette/CasseNoisette/GenerateDictionaryDialog.h
@@ -1,35 +1,41 @@
 #pragma once
 
-#include <QtWidgets/QDialog>
 #include "ui_GenerateDictionaryDialog.h"
 #include <memory>
-#include <QThread>
-#include "GenerateDictionaryWorker.h"
 
-class GenerateDictionaryDialog :
-	public QDialog
+class GenerateDictionaryWorker;
+class QThread;
+
+class GenerateDictionaryDialog : public QDialog
 {
 	Q_OBJECT
-	std::unique_ptr<QThread> generateDictionaryWorkerThread;
-	std::unique_ptr<GenerateDictionaryWorker> generateDictionaryWorker;
+
 public:
-	GenerateDictionaryDialog(QWidget * parent = 0);
+	GenerateDictionaryDialog(QWidget* parent = 0);
 	~GenerateDictionaryDialog();
-public slots:
+
+signals:
+	void generateDictionary();
+
+private slots:
+	// Actions des contrôles du GUI
 	void on_btnGenerateDictionary_clicked();
 	void on_btnCloseDialog_clicked();
 	void on_btnDictionaryFileName_clicked();
 	void on_spinMaxCombinationLength_valueChanged();
+
+	// Réactions face aux signaux de GenerateDictionaryWorker
 	void generationStarted();
 	void generationFinished();
 	void errorString(QString error);
-signals:
-	void generateDictionary();
 
 private:
 	bool AllInputsValid();
 
 	Ui::GenerateDictionaryDialog ui;
+	std::unique_ptr<QThread> generateDictionaryWorkerThread;
+	std::unique_ptr<GenerateDictionaryWorker> generateDictionaryWorker;
+
 	bool ignoreFutureWarnings = false;
 };
 
