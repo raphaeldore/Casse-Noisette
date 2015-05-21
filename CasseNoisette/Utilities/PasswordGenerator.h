c740a1ca2239e7ce87976174804a7024440e346a
Raphaël Doré <rdore@neomailbox.ch>
2015-05-21 16:15:39 -0400

    Ajout de la classe PasswordGenerator. Génère un mot de passe aléatoire d'une certaine longueur à partir d'un charset fourni préalablement.

---
+++ CasseNoisette/Utilities/PasswordGenerator.h
@@ -0,0 +1,8 @@
+#pragma once
+class PasswordGenerator
+{
+public:
+	static std::string GeneratePassword(const std::string& _charset, const unsigned int& _lenght);
+private:
+	static char GenerateRandomCharacter(const std::string& _charset);
+};
